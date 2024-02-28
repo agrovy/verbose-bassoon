7bd5c3bb7fdee98d56b14e8274cd76f52e351fe0
Dhruv <adj7807@gmail.com>
2024-02-28 20:19:47 +0000

    removed poll and twitter

--- www/mysite/mysite/urls.py
+++ www/mysite/mysite/urls.py
@@ -20,8 +20,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('polls/', include('polls.urls')),
-    path('twitter/', include('twitter.urls')),
+    #path('polls/', include('polls.urls')),
+    #path('twitter/', include('twitter.urls')),
     path('', include('gamesfinder.urls'))
 ]
 
